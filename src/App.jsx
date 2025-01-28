e70d1b93b3f866a61c3fcc835113a766a1f6e9b7
HiYasin <hello.mr.faisal@gmail.com>
2025-01-28 06:47:57 +0600

    Initial project setup completed with tailwind+daisyui+router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ThemeController from './ThemeController'
+
+function App() {
+  return (
+    <>
+    <div className='text-xl font-bold text-center'>
+      hello world
+      <button className="btn">Default</button>
+      <ThemeController></ThemeController>
+    </div>
+    </>
+  )
+}
+
+export default App
