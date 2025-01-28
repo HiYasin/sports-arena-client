6e8d6a942c5a0a162a4715bf86d2baf7c7688b4c
HiYasin <hello.mr.faisal@gmail.com>
2025-01-29 01:53:32 +0600

    all equipment page updated

--- src/pages/AllEquipment.jsx
+++ src/pages/AllEquipment.jsx
@@ -20,7 +20,7 @@ const Services = () => {
         <>
             <SectionTitle>All Equipments</SectionTitle>
             {/* table */} 
-            <table className="table-auto w-full bg-base-100 rounded-2xl overflow-hidden">
+            <table className="table-auto w-full bg-base-100 rounded-2xl overflow-x-scroll">
                 <thead>
                     <tr className='rounded-lg'>
                         <th className="px-4 py-2">Image</th>
@@ -28,21 +28,28 @@ const Services = () => {
                         <th className="px-4 py-2">Category</th>
                         <th className="px-4 py-2">Price</th>
                         <th className="px-4 py-2">Rating</th>
+                        <th className="px-4 py-2">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {products && products.map((product, index) => (
-                        <tr key={index}>
-                            <td className="border border-gray-400 px-4 py-2 flex justify-center">
+                        <tr key={index} className={index%2===0?'bg-gray-200':'bg-base-100'}>
+                            <td className="px-4 py-2 flex justify-center">
                                 <img src={product.image} alt={product.itemName} className="w-16 h-16 object-cover" />
                             </td>
-                            <td className="border border-gray-400 px-4 py-2">{product.itemName}</td>
-                            <td className="border border-gray-400 px-4 py-2">{product.categoryName}</td>
-                            <td className="border border-gray-400 px-4 py-2">${product.price}</td>
-                            <td className="border border-gray-400 px-4 py-2">{product.rating}</td>
+                            <td className="px-4 text-center py-2">{product.itemName}</td>
+                            <td className="px-4 text-center py-2">{product.categoryName}</td>
+                            <td className="px-4 text-center py-2">${product.price}</td>
+                            <td className="px-4 text-center py-2">{product.rating}</td>
+                            <td className="px-4 text-center py-2">
+                            <button className="btn btn-accent w-full">View Details</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
+                <tfoot className='h-10'>
+
+                </tfoot>
             </table>
         </>
     );
